48ef098b8042668e5de602d9787612c68e6f4c62
Collin Pierre <Malikishome@gmail.com>
2026-04-26 15:08:16 -0400

    Created the structure for the form page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,22 @@
 function Login() {
+    
     return (
         <div className="flex items-center justify-center h-screen">
         <h1 className="text-4xl font-bold">Login Page</h1>
+        <section>
+            <h2>Sign In</h2>
+            <form>
+                <label htmlFor="email">Email:</label>
+                <input type="email" id="email" name="email" required />
+                <br />
+                <label htmlFor="password">Password:</label>
+                <input type="password" id="password" name="password" required />
+                <br />
+                <button type="submit">Login</button>
+            </form>
+            <p>Don't have an account? <a href="/register">Sign Up</a></p>
+
+        </section>
         {/* Add your login form here :
         <LoginForm />
         */}
