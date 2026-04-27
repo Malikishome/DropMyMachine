b19d4383677f28e022222c083457587f34c19fd3
Collin Pierre <Malikishome@gmail.com>
2026-04-27 10:45:58 -0400

    Updated home page to display the right info

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-// Look over and make sure everything is good and working
+// TO-DO: make it look nice with tailwindcss
 function Home() {
 
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
         <h1 className="text-3xl text-center font-bold">Welcome to DropMyMachine</h1>
-        <p>Discover the perfect location for your machine learning model.</p>
+        <p>The fastest way to connect machine owners with high-traffic locations.</p>
         <Link to="/login"><button className="bg-blue-500 text-white px-4 py-2 rounded mt-4 hover:bg-blue-600">Login</button>
         </Link>
         <Link to="/register"><button className='bg-blue-500 text-white px-4 py-2 rounded mt-4 hover:bg-blue-600'>Register</button>
