dca26c0ca491ef12a13390084a1584d1e879b03c
Collin Pierre <Malikishome@gmail.com>
2026-05-02 02:32:58 -0400

    connect the auth account and saves it to the profiles table

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -24,17 +24,27 @@ router.post('/login', async (req, res) => {
 });
 
 router.post('/signup', async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password, role, full_name } = req.body;
 
   try {
     const { data, error } = await supabase.auth.signUp({
       email,
       password,
-      role,
-    });
+    })
 
     if (error) {
       return res.status(400).json({ message: error.message });
+    }
+
+    // This will save roles and the full names to the profiles table
+    const  { error: profileError } = await supabase.from('profiles').insert({
+      id: data.user.id,
+      role,
+      full_name,
+    });
+
+    if (profileError) {
+      return res.status(400).json({ message: profileError.message });
     }
 
     res.json({ user: data.user, session: data.session });
