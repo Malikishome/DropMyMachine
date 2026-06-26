2e6dec3fac59deaef39adcac2257c94e408cf08c
Collin Pierre <Malikishome@gmail.com>
2026-06-25 22:18:41 -0400

    Fixed the bug in the floating inputs

--- client/src/FloatingInput.jsx
+++ client/src/FloatingInput.jsx
@@ -12,9 +12,11 @@ function FloatingInput({ label, type = "text", name, value, onChange }) {
 
                 <label 
                 htmlFor={name}
-               className="absolute left-3 top-4 text-gray-500 transition-all duration-200 ease-in-out
-                    peer-focus:top-1.75 peer-focus:text-[11px] peer-focus:text-amber-500 peer-focus:font-medium
-                    peer-[:not(:placeholder-shown)]:top-1.75 peer-[not(:placeholder-shown)]:text-[11px] peer-[not(:placeholder-shown)]:text-gray-500 peer-[not(:placeholder-shown)]:font-medium">
+                    className="absolute left-3 top-4 text-gray-500 transition-all duration-200 ease-in-out pointer-events-none
+                    peer-focus:top-[7px] peer-focus:text-[11px] peer-focus:text-amber-500 peer-focus:font-medium
+                    peer-[:not(:placeholder-shown)]:top-[7px] peer-[:not(:placeholder-shown)]:text-[11px]
+                    peer-[:not(:placeholder-shown)]:text-gray-500 peer-[:not(:placeholder-shown)]:font-medium"
+>
                     {label}
                     </label>
         </div>
