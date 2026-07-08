2b3ec307f7cdd2d9f21a34db8797ef81d3f323e0
Collin Pierre <Malikishome@gmail.com>
2026-07-08 19:54:32 -0400

    Updated the backend so the server can run with no errors

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
+const listingRoutes = require('./routes/listingRoutes');
 
-const authroutes = require('./routes/authRoutes');
 const app = express();
 
 // This is the middleware section where we set up CORS and JSON parsing for incoming requests
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', authroutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/listings', listingRoutes);
 
 app.get('/', (req, res) => {
   res.send('Welcome to the DropMyMachine API!');
